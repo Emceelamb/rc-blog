3f26b2af813db84ba14030c4b942017000d4c015
emceelamb <mlam@berkeley.edu>
2020-08-10 19:33:54 -0400

    RC BLOG

---
+++ README.md
@@ -0,0 +1,6 @@
+# Recurse Center Process Blog
+## Fall 2020
+
+This is a process blog for my batch at Recurse Center.
+
+Bootstrapped with Gatsby Lumen
---
+++ config.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  pathPrefix: '/',
+  title: 'Mark\'s RC Blog',
+  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: '',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Process',
+      path: '/'
+    },
+    {
+      label: 'markofthelam.com',
+      path: 'https://markofthelam.com'
+    },
+  ],
+  author: {
+    name: 'Mark Lam',
+    photo: '/photo.jpg',
+    bio: 'This is my Recurse Center Process Blog for the Fall 1 2020 batch!',
+    contacts: {
+      email: '',
+      facebook: '',
+      telegram: '',
+      twitter: 'markofthelam',
+      github: 'emceelamb',
+      rss: '',
+      vkontakte: '',
+      linkedin: 'markofthelam',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
+};
